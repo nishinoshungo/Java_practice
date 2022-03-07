8c968016ce2197389cc93c7783a5a31a46ae302f
nishinoshungo <nishishun50@gmail.com>
2022-03-08 07:46:20 +0900

    add file

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Nishino";
+		System.out.println(name);
+		name = "David";
+		System.out.println(name);
+		
+		final int NUMBER = 7;
+		System.out.println(NUMBER);
+	}
+}
