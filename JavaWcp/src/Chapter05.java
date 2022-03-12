b2823268db317db02afea172db9cbdd2661b74fe
nishinoshungo <nishishun50@gmail.com>
2022-03-12 09:47:33 +0900

    add file

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long n1 = 444444444444444L;
+		double n2 = 4.56;
+		boolean x = true;
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(x);
+		
+		Float f = 1.22f;
+		Integer i = 100;
+		Character c = 'お';
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		
+		int[] array1 = {45, 23, 2, 5, 98};
+		System.out.println(array1[3]);
+	}
+}
---
+++ JavaWcp/src/Null01.java
@@ -0,0 +1,12 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		string1 = "あいう";
+		System.out.println(string1 + "の文字数:" + string1.length());
+		String1 = "";
+		System.out.println(string1 + "の文字数:" + string1.length());
+		String1 = null;
+		System.out.println(string1 + "の文字数:" + string1.length());
+	}
+}
