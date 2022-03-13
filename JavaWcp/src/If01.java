3cac8de6ffba7dd71be4a134b668c31fb1a603df
nishinoshungo <nishishun50@gmail.com>
2022-03-14 07:02:09 +0900

    add file

---
+++ JavaWcp/src/If01.java
@@ -0,0 +1,13 @@
+
+public class If01 {
+	public static void main(String[] args) {
+		int number = 6;
+		if(number < 5) {
+			System.out.println(number + "は5未満です");
+		} else if(number < 10) {
+			System.out.println(number + "は5以上、10未満です");
+		} else {
+			System.out.println(number + "は10以上です");
+		}
+	}
+}
