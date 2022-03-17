3c2507358ebd5eadcb22527a53e96e3f778b2c41
nishinoshungo <nishishun50@gmail.com>
2022-03-18 06:27:25 +0900

    add file

--- JavaWcp/src/classMethod/ClassMethod05.java
+++ JavaWcp/src/classMethod/ClassMethod05.java
@@ -3,7 +3,6 @@ import classMethod.human.Human05;
 public class ClassMethod05 {
 	public static void main(String[] args) {
 		Human05 yamada = new Human05();
-		System.out.println(yamada.name);
 		System.out.println(yamada.getName() + yamada.getAge());
 	}
 }
