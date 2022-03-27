0c9c71dbca50f53153f3230f1730453e2ef3d596
nishinoshungo <nishishun50@gmail.com>
2022-03-27 09:31:40 +0900

    add file

---
+++ JavaWcp/src/practice/Chapter15_1.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class Chapter15_1 {
+	public static void main(String[] args) {
+		int a = 976;
+		int b = 427;
+		System.out.println(a + "と" + b + "の最大公約数は" + gcd(a, b) + "です。");
+	}
+	
+//	static int gcd(int a, int b) {
+//		int bigNum = a;
+//		int smallNum = b;
+//		int amari;
+//		
+//		while(bigNum % smallNum != 0) {
+//			amari = bigNum % smallNum;
+//			bigNum = smallNum;
+//			smallNum = amari;
+//		}
+//		return smallNum;
+//	}
+	
+	static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+	
+	
+}
